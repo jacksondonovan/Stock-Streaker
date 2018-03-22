ad354af752a8a46a9d2cb032a0b02c621049d4ac
Jackson Donovan <iinventedtwitter@gmail.com>
2018-03-21 23:19:52 -0600

    works now without react bootstrap

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Navbar, Nav, NavItem, MenuItem } from 'react-bootstrap';
-
-export default class Navbar extends React.Component {
-  render() {
-    return (
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#home">Stock Streaker</a>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavItem eventKey={1} href="#">
-            Home
-          </NavItem>
-          <NavItem eventKey={2} href="#">
-            About
-          </NavItem>
-        </Nav>
-      </Navbar>
-    );
-  }
-}
+// import React from 'react';
+// import { Navbar, Nav, NavItem, MenuItem } from 'react-bootstrap';
+//
+// export default class Navbar extends React.Component {
+//   render() {
+//     return (
+//       <Navbar>
+//         <Navbar.Header>
+//           <Navbar.Brand>
+//             <a href="#home">Stock Streaker</a>
+//           </Navbar.Brand>
+//         </Navbar.Header>
+//         <Nav>
+//           <NavItem eventKey={1} href="#">
+//             Home
+//           </NavItem>
+//           <NavItem eventKey={2} href="#">
+//             About
+//           </NavItem>
+//         </Nav>
+//       </Navbar>
+//     );
+//   }
+// }
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,17 @@
-console.log('it works!');
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Here is first text
+      </div>
+    );
+  }
+}
+
+render (
+  <App />,
+  window.document.getElementById('root')
+);
