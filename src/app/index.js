d6fa73f13c98ac327bb69e469796e52447caf780
Jackson Donovan <iinventedtwitter@gmail.com>
2018-03-23 22:57:50 -0600

    adding react dom router

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <Router>
         <Navbar />
-        <Home />
-      </div>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+      </Router>
     );
   }
 }
