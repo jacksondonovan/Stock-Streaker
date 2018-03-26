e88975c4c65021229dc717b6052b2fbe076f86a2
Jackson Donovan <iinventedtwitter@gmail.com>
2018-03-26 14:44:37 -0600

    webpack wont run with react router dom module

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -11,10 +11,10 @@ export default class Navbar extends React.Component {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
+              <a className="nav-link" href="/about">About</a>
             </li>
           </ul>
         </div>
--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Router, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -8,11 +7,9 @@ import About from './components/About';
 class App extends React.Component {
   render() {
     return (
-      <Router>
+      <div>
         <Navbar />
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-      </Router>
+      </div>
     );
   }
 }
